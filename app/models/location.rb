b9801787faa6f41eb89a942aaa965dc292e4f4ce
codercreighton <codercreighton@gmail.com>
2018-08-06 10:27:23 -0400

    first commit

---
+++ app/models/location.rb
@@ -0,0 +1,11 @@
+class Location < ApplicationRecord
+	geocoded_by :my_location
+
+	validates :address, presence: true
+	validates :city, presence: true
+	after_validation :geocode
+
+	def my_location
+		"#{address}, #{city}, GA"
+	end	
+end
